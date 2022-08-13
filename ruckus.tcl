88175c22f30cfd104f0e9506a68e481873ad6872
rene brglez <rene.brglez@cosylab.com>
2022-08-13 18:31:39 +0200

    Commented unused folders from ruckus.tcl

--- ruckus.tcl
+++ ruckus.tcl
@@ -4,8 +4,8 @@ source $::env(RUCKUS_QUIET_FLAG) $::env(RUCKUS_PROC_TCL)
 # Load ruckus files
 loadRuckusTcl "$::DIR_PATH/axi"
 loadRuckusTcl "$::DIR_PATH/base"
-loadRuckusTcl "$::DIR_PATH/dsp"
-loadRuckusTcl "$::DIR_PATH/devices"
-loadRuckusTcl "$::DIR_PATH/ethernet"
-loadRuckusTcl "$::DIR_PATH/protocols"
-loadRuckusTcl "$::DIR_PATH/xilinx"
+#loadRuckusTcl "$::DIR_PATH/dsp"
+#loadRuckusTcl "$::DIR_PATH/devices"
+#loadRuckusTcl "$::DIR_PATH/ethernet"
+#loadRuckusTcl "$::DIR_PATH/protocols"
+#oadRuckusTcl "$::DIR_PATH/xilinx"
